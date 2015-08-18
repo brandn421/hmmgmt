d721e0e55c1a13390ad9f019e97796928db28563
brandn421 <brandn421@gmail.com>
2015-08-17 23:08:14 -0400

    Responsive design changes.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,11 @@ jQuery("document").ready(function($){
         $('.menu__dropdown').fadeOut('fast');
     });
 
+    // Mobile Navigation
+    $('.menu__mobile').click(function () {
+        $('.menu__dropdown--mobile').toggle();
+    });
+
     // Autoscroll to Home Page Map
     $('.intro__button').click(function() {
    		$.scrollTo($('.cities').position().top - 220, 600);
